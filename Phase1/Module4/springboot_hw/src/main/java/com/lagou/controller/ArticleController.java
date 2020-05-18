18d25dee5144c3caa66d0d47723afe9856c742b1
Yusi Huang <yusihuang@Yusis-MacBook-Pro.local>
2020-05-18 12:37:33 -0700

    Phase1-Module4,作业 修正前端路径让渲染顺利

--- Phase1/Module4/springboot_hw/src/main/java/com/lagou/controller/ArticleController.java
+++ Phase1/Module4/springboot_hw/src/main/java/com/lagou/controller/ArticleController.java
@@ -4,23 +4,25 @@ import com.lagou.pojo.Article;
 import com.lagou.service.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
-@RestController
+@Controller
 public class ArticleController {
 
     @Autowired
     private ArticleService articleService;
 
     @RequestMapping("/findAll")
-    public Page<Article> findAllArticles() {
+    public String findAllArticles(Model model) {
         int page = 0;
         int size = 2;
 
-        return articleService.findAllArticles(page, size);
+        Page<Article> articles = articleService.findAllArticles(page, size);
+        model.addAttribute("articles", articles);
+        return "/client/index";
     }
 
     @RequestMapping("/findOne")
