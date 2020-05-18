44b4968f99d8945aa95eff80d9b4fc93435a3c7e
Yusi Huang <yusihuang@Yusis-MacBook-Pro.local>
2020-05-18 10:40:47 -0700

    Phase1-Module4 作业, Add pagination in backend

--- Phase1/Module4/springboot_hw/src/main/java/com/lagou/controller/ArticleController.java
+++ Phase1/Module4/springboot_hw/src/main/java/com/lagou/controller/ArticleController.java
@@ -3,6 +3,7 @@ package com.lagou.controller;
 import com.lagou.pojo.Article;
 import com.lagou.service.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,8 +16,11 @@ public class ArticleController {
     private ArticleService articleService;
 
     @RequestMapping("/findAll")
-    public List<Article> findAllArticles() {
-        return articleService.findAllArticles();
+    public Page<Article> findAllArticles() {
+        int page = 0;
+        int size = 2;
+
+        return articleService.findAllArticles(page, size);
     }
 
     @RequestMapping("/findOne")
--- Phase1/Module4/springboot_hw/src/main/java/com/lagou/service/ArticleService.java
+++ Phase1/Module4/springboot_hw/src/main/java/com/lagou/service/ArticleService.java
@@ -3,9 +3,11 @@ package com.lagou.service;
 import com.lagou.pojo.Article;
 import com.lagou.repository.ArticleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -14,8 +16,9 @@ public class ArticleService {
     @Autowired
     private ArticleRepository articleRepository;
 
-    public List<Article> findAllArticles() {
-        return articleRepository.findAll();
+    public Page<Article> findAllArticles(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        return articleRepository.findAll(pageable);
     }
 
     public Article findArticleById(Integer id) {
