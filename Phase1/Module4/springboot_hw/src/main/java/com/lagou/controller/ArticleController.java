e6386e3a54205d5bc8b7f1c01e81161feb75e531
Yusi Huang <yusihuang@Yusis-MacBook-Pro.local>
2020-05-18 15:23:30 -0700

    Phase1-Module4, 作业 创建pagination 并渲染

--- Phase1/Module4/springboot_hw/src/main/java/com/lagou/controller/ArticleController.java
+++ Phase1/Module4/springboot_hw/src/main/java/com/lagou/controller/ArticleController.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 
 @Controller
 public class ArticleController {
@@ -16,9 +18,16 @@ public class ArticleController {
     private ArticleService articleService;
 
     @RequestMapping("/findAll")
-    public String findAllArticles(Model model) {
+    public String findAllArticles(HttpServletRequest request, Model model) {
         int page = 0;
         int size = 2;
+
+        if (request.getParameter("page") != null && !request.getParameter("page").isEmpty()) {
+            page = Integer.parseInt(request.getParameter("page")) - 1;
+        }
+        if (request.getParameter("size") != null && !request.getParameter("size").isEmpty()) {
+            size = Integer.parseInt(request.getParameter("size"));
+        }
 
         Page<Article> articles = articleService.findAllArticles(page, size);
         model.addAttribute("articles", articles);
